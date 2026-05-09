3de7e708a5c8016356505b06ba8f6e61896dfe96
Y-Sagara <consomme_ior14310@docomo.ne.jp>
2026-05-10 01:06:57 +0900

    feat: add answers of worldcup

---
+++ insert_data.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+TRUNCATE_RESULTS=$($PSQL "truncate table games, teams")
+cat games.csv | while IFS=, read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR = "year" ]]
+  then
+    continue  
+  else
+    INSERT_WINNER_RESULT=$($PSQL "insert into teams(name) values ('$WINNER')")
+    INSERT_OPPONENT_RESULT=$($PSQL "insert into teams(name) values ('$OPPONENT')")
+    WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    INSERT_GAMES_RESULT=$($PSQL "insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+done
